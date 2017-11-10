6f660909d32189901c85eed8e3bcbfb41458f775
“Chupacabra1985 <jan.roztropinski@op.pl>
2017-11-10 21:17:02 +0100

    Second Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TsetingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TsetingMain.java
@@ -1,7 +1,29 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.Calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TsetingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        }else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int resultAdd = calculator.add(4,5);
+        int resultSubstract = calculator.substract(4,5);
+
+        if(resultAdd>resultSubstract){
+            System.out.println("Calculator working OK");
+        }else{
+            System.out.println("Calculator Error!");
+        }
     }
 }
