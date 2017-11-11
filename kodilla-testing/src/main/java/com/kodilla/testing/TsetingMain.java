fa59c9067101b972f7a7ce7e0339b05016f81787
“Chupacabra1985 <jan.roztropinski@op.pl>
2017-11-11 15:54:14 +0100

    First Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TsetingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TsetingMain.java
@@ -1,29 +1,36 @@
 package com.kodilla.testing;
 
-import com.kodilla.testing.Calculator.Calculator;
+import com.kodilla.testing.calculator.Calculator;
 import com.kodilla.testing.user.SimpleUser;
 
 public class TsetingMain {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         SimpleUser simpleUser = new SimpleUser("theForumUser");
 
         String result = simpleUser.getUsername();
 
-        if (result.equals("theForumUser")){
+        if (result.equals("theForumUser")) {
             System.out.println("test OK");
-        }else {
+        } else {
             System.out.println("Error!");
         }
 
         Calculator calculator = new Calculator();
 
-        int resultAdd = calculator.add(4,5);
-        int resultSubstract = calculator.substract(4,5);
+        int resultAdd = calculator.add(4, 5);
+        int resultSubstract = calculator.substract(5, 4);
 
-        if(resultAdd>resultSubstract){
-            System.out.println("Calculator working OK");
-        }else{
-            System.out.println("Calculator Error!");
+        if (resultAdd == 9) {
+            System.out.println("Calculator Add working OK");
+        } else {
+            System.out.println("Calculator Error Add!");
+        }
+
+        if (resultSubstract == 1) {
+            System.out.println("Calculator Substract working OK");
+        } else {
+            System.out.println("Calculator Error Substract!");
         }
     }
 }
+
