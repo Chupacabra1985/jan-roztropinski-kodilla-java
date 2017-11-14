d86b8fb5a4966b7d2f10b0619c399dc0dc3da639
“Chupacabra1985 <jan.roztropinski@op.pl>
2017-11-14 23:19:36 +0100

    Second Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -1,4 +1,24 @@
 package com.kodilla.testing.shape;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ShapeCollector {
+
+    private List<Shape> listShape = new ArrayList<Shape>();
+
+    public void addFigure(Shape shape){
+
+    }
+
+    public void removeFigure(int n){
+
+    }
+
+    public void showFigures(){
+
+    }
+
+
+
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/shape/ShapeCollectorTestSuite.java
@@ -1,4 +1,30 @@
 package com.kodilla.testing.shape;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 public class ShapeCollectorTestSuite {
+
+    @Before
+    public void before(){
+        System.out.println("Starting test.")
+    }
+
+    @After
+    public void after(){
+        System.out.println("End test.")
+    }
+
+    @Test
+    public void testAddFigure(){
+        //Given
+        Shape shape = new Shape(Circle);
+
+        //When
+        shapeCollector.addFigure(shape);
+
+
+    }
+
 }
