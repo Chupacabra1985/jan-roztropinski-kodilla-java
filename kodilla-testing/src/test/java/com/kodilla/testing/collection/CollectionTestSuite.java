3b8ab7ba91db26746f00f36bf956c4eb93a0aa21
“Chupacabra1985 <jan.roztropinski@op.pl>
2017-11-14 19:12:05 +0100

    FirstCommit

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -21,49 +21,40 @@ public class CollectionTestSuite {
 
 
 
-    ArrayList<Integer> numberList = new ArrayList<Integer>(
-            Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+
 
     @Test
     public void testOddNumbersExterminatorEmptyList(){
-        numberList.clear();
-        System.out.println("Elements in numberList: " + numberList.size());
-        Integer listSize = numberList.size();
+
         //Given
         OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
-        oddNumbersExterminator.exterminate(numberList);
+
 
         //When
-        Integer result = oddNumbersExterminator.getNewList().size();
-        System.out.println("Elements in newList: " + oddNumbersExterminator.getNewList().size());
+        oddNumbersExterminator.exterminate(new ArrayList<>());
+
 
         //Then
-        Assert.assertEquals(listSize, result);
+        int  result = oddNumbersExterminator.getNewList().size();
+        Assert.assertEquals( 0, result);
 
     }
 
     @Test
     public void testOddNumbersExterminatorNormalList(){
-        System.out.println("Elements in numberList: " + numberList.size());
-        Integer listSize = numberList.size();
-
         //Given
+        ArrayList<Integer> numberList = new ArrayList<Integer>(
+                Arrays.asList(1,2,3,4,5,6,7,8,9,10));
         OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
-        oddNumbersExterminator.exterminate(numberList);
+
 
         //When
-        Integer result = oddNumbersExterminator.getNewList().size();
-        System.out.println("Elements in newList: " + oddNumbersExterminator.getNewList().size());
-        for(Integer element: numberList){
-            System.out.println("Element in numberList: " + element);
-        }
+        oddNumbersExterminator.exterminate(numberList);
 
-        for(Integer newElement: oddNumbersExterminator.getNewList()){
-            System.out.println("Element in newList: " + newElement);
-        }
 
         //Then
-        Assert.assertNotEquals(listSize, result);
+        int result = oddNumbersExterminator.getNewList().size();
+        Assert.assertEquals(5, result);
 
     }
 
