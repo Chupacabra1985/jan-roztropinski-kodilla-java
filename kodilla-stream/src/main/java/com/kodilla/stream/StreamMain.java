5633713defbb8f96e8cc53103f6458e5b1aed008
“Chupacabra1985 <jan.roztropinski@op.pl>
2017-11-19 19:30:07 +0100

    New Module

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,32 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+    public static void main(String[] args) {
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3,4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3,4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3,4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3,4, FunctionalCalculator::divideAByB);
+
+        System.out.println("Poem Beautifier");
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        poemBeautifier.beautify("Wykrzyknik",tekst -> "! "+tekst+" !");
+        poemBeautifier.beautify("Litery",tekst -> "ABC "+tekst+" ABC");
+        poemBeautifier.beautify("Rownosc", tekst -> "=="+tekst+"==");
+        poemBeautifier.beautify("duze litery", tekst -> tekst.toUpperCase());
+    }
+
+}
