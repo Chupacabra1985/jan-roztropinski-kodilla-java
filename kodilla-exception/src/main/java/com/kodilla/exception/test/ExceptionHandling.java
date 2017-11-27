83eed4a9afed0e1338a273c57fb2dade7db05036
“Chupacabra1985 <jan.roztropinski@op.pl>
2017-11-27 21:30:09 +0100

    ThrowThrows

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,5 +1,19 @@
 package com.kodilla.exception.test;
 
+import java.io.IOException;
+
 public class ExceptionHandling{
+
+    public static void main(String[] args) throws Exception {
+        SecondChallenge secondChallenge = new SecondChallenge();
+                try{
+                    String result = secondChallenge.probablyIWillThrowException(1.5, 2.0);
+                    System.out.println(result);
+                }catch (IOException e){
+                    throw new Exception();
+                }finally {
+                    System.out.println("Finally");
+                }
+    }
 
 }
