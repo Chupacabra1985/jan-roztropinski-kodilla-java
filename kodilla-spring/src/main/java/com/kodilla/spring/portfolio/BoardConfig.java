8f3182cb562da6d0902e4a9f8fbddea8ddc3b2cf
“Chupacabra1985 <jan.roztropinski@op.pl>
2017-12-13 00:06:22 +0100

    Board

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -0,0 +1,23 @@
+package com.kodilla.spring.portfolio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class BoardConfig {
+
+    @Autowired
+    @Qualifier("board1")
+    Board board;
+
+    @Bean(name = "board1")
+    @Scope("prototype")
+    public Board boardOne(){
+
+    }
+
+
+}
