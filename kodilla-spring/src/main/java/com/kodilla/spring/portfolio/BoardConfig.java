ba3ff097f214bb96e72afa1f49e9130ac7624d7e
“Chupacabra1985 <jan.roztropinski@op.pl>
2017-12-16 00:19:45 +0100

    Portfolio

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -9,6 +9,29 @@ import org.springframework.context.annotation.Scope;
 @Configuration
 public class BoardConfig {
 
+    @Bean
+    public Board getBoard(){
+        return new Board(getToDoList(), getInProgressList(), getDoneList());
+    }
+
+    @Bean(name="toDoList")
+    @Scope("prototype")
+    public TaskList getToDoList(){
+        return new TaskList();
+    }
+
+    @Bean(name="inProgressList")
+    @Scope("prototype")
+    public TaskList getInProgressList(){
+        return new TaskList();
+    }
+
+    @Bean(name="doneLsit")
+    @Scope("prototype")
+    public TaskList getDoneList(){
+        return new TaskList();
+    }
+
 
 
 }
--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/TaskList.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/TaskList.java
@@ -6,8 +6,8 @@ import java.util.List;
 public class TaskList {
         final List<String> tasks;
 
-        public TaskList(List<String> tasks) {
-            this.tasks = new ArrayList<>();
+        public TaskList() {
+            tasks = new ArrayList<>();
         }
     }
 
