d821a0121dd96161e1a4c509f5d0830463a956d2
“Chupacabra1985 <jan.roztropinski@op.pl>
2018-03-28 14:13:03 +0200

    RpsSecond

--- kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
@@ -8,7 +8,7 @@ public class RpsRunner {
 
         Scanner keyboard = new Scanner(System.in);
         Random rand = new Random();
-        int comScore=0;
+        int comScore = 0;
         int userScore = 0;
         int rounds = 0;
 
@@ -19,43 +19,74 @@ public class RpsRunner {
 
         System.out.println("Write my your name: ");
         String name = keyboard.nextLine();
-        boolean nameTrue = name.length()>=3;
+        boolean nameTrue = name.length() >= 3;
 
-        if(nameTrue==true) {
+        if (nameTrue == true) {
             System.out.println("Do you want to start a new game? (Y/N)");
             String play = keyboard.nextLine();
-            startGame = play.equals("Y");}
-            if(startGame == true) {
-                System.out.println("How many rounds: ");
-               rounds = keyboard.nextInt();
-               newGame = rounds>0;
-            }
-            if(newGame==true){
+            startGame = play.equalsIgnoreCase("Y");
+        }
+        if (startGame == true) {
+            System.out.println("How many rounds: ");
+            rounds = keyboard.nextInt();
+            newGame = rounds > 0;
+        }
+        if (newGame == true) {
 
-            while (startGame && comScore<rounds && userScore<rounds){
+            while (startGame && comScore < rounds && userScore < rounds) {
                 System.out.println("Enter 1 for ROCK, 2 for PAPER, 3 for SCISSORS, x for END GAME, n NEW GAME");
-                int user = keyboard.nextInt();
-                int comp = rand.nextInt(2) + 1;
-                System.out.println("I draw: " + comp);
-                if((user==1 && comp==3)||(user==2 && comp==1)||(user==3 && comp==2)){
-                    System.out.println("You win!");
-                    userScore++;
-                }else if ((user==1 && comp==2)||(user==2 && comp==3)||(user==3 && comp==1)){
-                    System.out.println("I win!");
-                    comScore++;
-                }
+                String user = keyboard.nextLine();
+                if (user.equalsIgnoreCase("X")) {
+                    System.out.println("Do you really want to end the game? (Y/N)");
+                    String close = keyboard.nextLine();
+                    if (close.equalsIgnoreCase("Y")) {
+                        System.out.println("Goodbye!");
+                        System.exit(0);
+                    } else {
+                        continue;
+                    }
+                } else if (user.equalsIgnoreCase("N")) {
+                    System.out.println("Do you really want to end current game? (Y/N)");
+                    String reloadGame = keyboard.nextLine();
+                    if (reloadGame.equalsIgnoreCase("Y")) {
+                        userScore = 0;
+                        comScore = 0;
+                        continue;
+                    } else {
+                        continue;
+                    }
+                } else if (user.equals("1") || user.equals("2") || user.equals("3")) {
+                    int comp = rand.nextInt(2) + 1;
+                    System.out.println("I draw: " + comp);
+                    if ((user.equals("1") && comp == 3) || (user.equals("2") && comp == 1) || (user.equals("3") && comp == 2)) {
+                        System.out.println("You win!");
+                        userScore++;
+                    } else if ((user.equals("1") && comp == 2) || (user.equals("2") && comp == 3) || (user.equals("3") && comp == 1)) {
+                        System.out.println("I win!");
+                        comScore++;
+                    }
 
-                System.out.println("SCORE  you: " + userScore + " my: " + comScore);
+                    System.out.println("SCORE  you: " + userScore + " my: " + comScore);
+                }
             }
 
-            if(comScore > userScore){
+            if (comScore > userScore) {
                 System.out.println("I bet you!");
-            }else {
+            } else {
                 System.out.println("You beat me!");
+            }
+
+            System.out.println("Do you want to play again? (Y/N)");
+            String play = keyboard.nextLine();
+            if (play.equalsIgnoreCase("y")) {
+                System.out.println("Please press play");
+
+            } else {
+                System.out.println("See you next time!");
             }
         }
 
     }
-
-
 }
+
+
